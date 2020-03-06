a91c6ee571036d3e07ab1c41145b63f5439c3a64
Sevima <romadhon28akbar@gmail.com>
2020-03-06 10:04:07 +0700

    Initial Commit

---
+++ News/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  News
+//
+//  Created by sevima on 06/03/20.
+//  Copyright © 2020 sevima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
